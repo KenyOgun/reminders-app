e226a32029f954b6d3204913ab883761df296946
KenyOgun <holleraturboi@yahoo.ca>
2023-01-09 23:40:11 -0500

    adjusted RemindersList to contain one Reminder element for each reminder in the list

--- src/RemindersList.js
+++ src/RemindersList.js
@@ -3,9 +3,21 @@ import Reminder from "./Reminder";
 function RemindersList(props) {
   return (
     <div>
-      <Reminder />
-      <Reminder />
-      <Reminder />
+      <Reminder
+        reminderText="Pick up Wesley"
+        dueDate="2364-01-15"
+        isComplete={false}
+      />
+      <Reminder
+        reminderText="Meet with Jean-Luc"
+        dueDate="2364-01-29"
+        isComplete={false}
+      />
+      <Reminder
+        reminderText="Holodeck time!"
+        dueDate="2364-06-01"
+        isComplete={false}
+      />
     </div>
   );
 }
